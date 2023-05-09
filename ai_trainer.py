62e403167db07a07f4f156c179b265a866320105
Shanein <samuelhanein@gmail.com>
2023-05-09 17:20:31 +0200

    add mlp model

--- ai_trainer.py
+++ ai_trainer.py
@@ -4,6 +4,8 @@ import numpy as np
 from keras import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
+from sklearn.neural_network import MLPClassifier
+
 import dataset
 
 img_length = 750
@@ -38,6 +40,7 @@ def start_training():
     test_x = dataset.format_dataset(test_x, img_length)
     test_y = np_utils.to_categorical(test_y, 2)
 
+    #choose your model
     return cnn_model(train_x, train_y, test_x, test_y)
 
 
@@ -56,6 +59,25 @@ def cnn_model(train_x, train_y, test_x, test_y):
     # training the model for 10 epochs
     model.fit(train_x, train_y, batch_size=128, epochs=10, validation_data=(test_x, test_y))
     score(model, test_x, test_y)
+
+    # Calculer la précision du modèle sur l'ensemble de test
+    test_acc = model.evaluate(test_x, test_y)[1]
+    print('Précision sur l\'ensemble de test :', test_acc)
+
+    return model
+
+def mlp_model(train_x, train_y, test_x, test_y):
+    # building a linear stack of layers with the sequential model
+    model = MLPClassifier(hidden_layer_sizes=(300,), verbose=2)
+
+    model.fit(train_x, train_y)
+
+    score(model, test_x, test_y)
+
+    # Calculer la précision du modèle sur l'ensemble de test
+    test_acc = model.score(test_x, test_y)
+    print('Précision sur l\'ensemble de test :', test_acc)
+
     return model
 
 def score(model, test_x, test_y):
@@ -67,6 +89,6 @@ def score(model, test_x, test_y):
     print('20 premieres données de test :\n', test_y_inverse[:20])
     print('20 premieres données de la prediction de test :\n', test_y_pred[:20])
 
-    # Calculer la précision du modèle sur l'ensemble de test
-    test_acc = model.evaluate(test_x, test_y)[1]
-    print('Précision sur l\'ensemble de test :', test_acc)
+    # # Calculer la précision du modèle sur l'ensemble de test
+    # test_acc = model.evaluate(test_x, test_y)[1]
+    # print('Précision sur l\'ensemble de test :', test_acc)
