239ff0e4d8ec8d3cf8cbb5c5746845df68eb195d
Shanein <samuelhanein@gmail.com>
2023-05-16 16:41:50 +0200

    Add vgg model and doc results

--- ai_trainer.py
+++ ai_trainer.py
@@ -1,14 +1,17 @@
 import pickle
 
 import numpy as np
-from keras import Sequential
-from keras.layers import Dense
+from keras import Sequential, Model
+from keras.applications import VGG19
+from keras.layers import Dense, Flatten
+from keras.optimizers import Adam
 from keras.utils import np_utils
 from sklearn.neural_network import MLPClassifier
 
 import dataset
 
-img_length = 750
+# img_length = 750
+img_length = 32
 
 
 def get_ai(regenerate=False):
@@ -41,7 +44,7 @@ def start_training():
     test_y = np_utils.to_categorical(test_y, 2)
 
     #choose your model
-    return cnn_model(train_x, train_y, test_x, test_y)
+    return vgg_model(train_x, train_y, test_x, test_y)
 
 
 def cnn_model(train_x, train_y, test_x, test_y):
@@ -79,6 +82,56 @@ def mlp_model(train_x, train_y, test_x, test_y):
     print('Précision sur l\'ensemble de test :', test_acc)
 
     return model
+
+def vgg_model(list_x, list_y, test_x, test_Y):
+
+    print('X (vecteur): vgg:  ' + str(list_x.shape))
+
+
+    #Redimonsionner en 32\
+    list_x= np.repeat(list_x[..., np.newaxis], 3, axis=-1)
+    list_x = np.array(list_x).reshape(-1, 32, 32, 3)
+    print('X (vecteur) vgg: ' + str(list_x.shape))
+
+    test_x= np.repeat(test_x[..., np.newaxis], 3, axis=-1)
+    test_x = np.array(test_x).reshape(-1, 32, 32, 3)
+
+
+    print('X (vecteur) vgg: ' + str(test_x.shape))
+
+
+    # Charger le modèle VGG19 pré-entraîné
+    base_model = VGG19(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
+
+    # Geler les poids des couches du modèle VGG19
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # Ajouter les nouvelles couches de classification
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dense(300, activation='relu')(x)
+    predictions = Dense(2, activation='softmax')(x)
+
+    # Créer le modèle final en spécifiant les entrées et les sorties
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Compiler le modèle avec une fonction de perte de catégorie croisée, un optimiseur Adam et la métrique de précision
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+
+    # Entraîner le modèle en utilisant les images d'entraînement et les étiquettes correspondantes
+    model.fit(list_x, list_y, batch_size=128, epochs=10, validation_data=(test_x, test_Y))
+
+    score(model, test_x, test_Y)
+
+    # Évaluer le modèle sur l'ensemble de test
+    test_loss, test_acc = model.evaluate(test_x, test_Y)
+    print('Perte sur l\'ensemble de test :', test_loss)
+    print('Précision sur l\'ensemble de test :', test_acc)
+
+
+    return model
+
 
 def score(model, test_x, test_y):
     test_y_pred = np.argmax(model.predict(test_x), axis=-1)
